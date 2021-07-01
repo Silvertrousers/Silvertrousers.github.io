3ff8741a2da1e083ee42dcb4c728a63932a74d0f
Silvertrousers <56201218+Silvertrousers@users.noreply.github.com>
2021-07-01 23:59:22 +0100

    Add files via upload

---
+++ precache-manifest.8e864f9195936c8857b9495582af883b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ceb09634c1fb537a45cbc016e6dd12b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "195e5181e5a246c7d836",
+    "url": "./static/css/2.cee0d1d4.chunk.css"
+  },
+  {
+    "revision": "f086f6329494a6115241",
+    "url": "./static/css/main.1686b9d4.chunk.css"
+  },
+  {
+    "revision": "195e5181e5a246c7d836",
+    "url": "./static/js/2.4c9ee8e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4c9ee8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f086f6329494a6115241",
+    "url": "./static/js/main.64fab1cf.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "cd6d11ef068a3f0d483a61b73044e4ea",
+    "url": "./static/media/defaultProfile.cd6d11ef.png"
+  },
+  {
+    "revision": "4ef8954d49b3a7ab17e7438cb365028b",
+    "url": "./static/media/defaultbanner.4ef8954d.png"
+  }
+]);
